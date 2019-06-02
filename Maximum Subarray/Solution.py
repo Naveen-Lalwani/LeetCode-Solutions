831f83504c95fcfb10ba29e106d5d7c8ebb1e9ed
NeoSky <naveenl@andrew.cmu.edu>
2019-06-01 20:32:07 -0400

    Update Solution.py

--- Maximum Subarray/Solution.py
+++ Maximum Subarray/Solution.py
@@ -6,14 +6,9 @@ Author: Naveen Lalwani
 '''
 class Solution:    
     def maxSubArray(self, nums: List[int]) -> int:
-        '''
-         Variable to store maximum subarray sum.
-         '''
-        summation = nums[0]
-        '''
-        Variable to store the sum on the subarray.
-        '''
-        maximum = nums[0]
+
+        summation = nums[0]     # Variable to store maximum subarray sum.
+        maximum = nums[0]       # Variable to store the sum on the subarray.
         
         '''
         Algorithm: We start with the second element in the list and we find if the new sum is greater than the current value or not, 
