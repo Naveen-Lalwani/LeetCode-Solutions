568421b0f81ea4a73b6f2e88b72c95ce3339dbf5
NeoSky <naveenl@andrew.cmu.edu>
2019-06-01 20:34:37 -0400

    Update Solution.py

--- Maximum Subarray/Solution.py
+++ Maximum Subarray/Solution.py
@@ -1,20 +1,18 @@
-"""
-Time Complexity: O(N)
-Space Complexity: O(1)
+# Time Complexity: O(N)
+# Space Complexity: O(1)
+#
+# Author: Naveen Lalwani
 
-Author: Naveen Lalwani
-"""
 class Solution:    
     def maxSubArray(self, nums: List[int]) -> int:
 
         summation = nums[0]     # Variable to store maximum subarray sum.
         maximum = nums[0]       # Variable to store the sum on the subarray.
         
-        """
-        Algorithm: We start with the second element in the list and we find if the new sum is greater than the current value or not, 
-        if not, we choose the current value which implies, we start a new subarray from this number forward. We then compare if the
-        current sum is bigger than the current maximum or not, and update maximum accordingly.
-        """
+        # Algorithm: We start with the second element in the list and we find if the new sum is greater than the current value or not, 
+        # if not, we choose the current value which implies, we start a new subarray from this number forward. We then compare if the
+        # current sum is bigger than the current maximum or not, and update maximum accordingly.
+        
         for i in range(1, len(nums)):
             summation = max(summation + nums[i], nums[i])
             maximum = max(summation, maximum)
